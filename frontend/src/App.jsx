ab027f0178f864295f661625076c96bd60051478
Arnav Sharda <asharda7898@gmail.com>
2025-01-14 20:26:31 +0530

    initialization error resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import { Layout } from "./layout/layout"
 import { CodeForces } from "./pages/codeforces"
 import { CodeChef } from "./pages/codechef"
-import { LeetCode } from "./pages/LeetCode"
+import { LeetCode } from "./pages/leetcode"
 import './App.css'
 import { ErrorPage } from "./pages/error"
 import { NotFound } from "./pages/NotFound"
